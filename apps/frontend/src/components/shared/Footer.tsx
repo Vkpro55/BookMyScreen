cc80a9da05809d9c3cf18d191e03b92be18677dc
Vkpro55 <vinodrao835@gmail.com>
2026-05-28 10:21:50 +0530

    feat(footer): implement footer section

--- apps/frontend/src/components/shared/Footer.tsx
+++ apps/frontend/src/components/shared/Footer.tsx
@@ -1,5 +1,44 @@
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaYoutube,
+  FaPinterestP,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import mainLogo from "../../assets/main-icon-white.png";
+
+import { Icon } from "@repo/ui/icon";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-[#2b2b2b] text-gray-400 text-sm">
+      <div className="border-t border-gray-600 w-full" />
+      <div className="flex flex-col items-center py-6">
+        {/* Logo */}
+        <img src={mainLogo} alt="BookMyScreen Logo" className="w-28 mb-4" />
+
+        {/* Social Icons */}
+        <div className="flex space-x-4 mb-4 mx-auto">
+          <Icon variant="social" Icon={FaTwitter} />
+          <Icon variant="social" Icon={FaLinkedinIn} />
+          <Icon variant="social" Icon={FaInstagram} />
+        </div>
+
+        {/* Copyright */}
+        <p className="text-center text-xs px-4 max-w-4xl">
+          Copyright 2025 © bookMyScreen Pvt Ltd. Ltd. All Rights Reserved.
+          <br />
+        </p>
+        <small>
+          The content and images used on this site are copyright protected and
+          copyrights vests with the respective owners. The usage of the content
+          and images on this website is intended to promote the works and no
+          endorsement of the artist shall be implied.
+        </small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- packages/ui/src/icon.tsx
+++ packages/ui/src/icon.tsx
@@ -3,21 +3,28 @@
 import { IconType } from "react-icons";
 
 interface IconProps {
-  type?: "gray";
+  variant?: "muted" | "social";
   size?: number;
   className?: string;
   Icon: IconType;
 }
 
 export const Icon = ({
-  type = "gray",
+  variant = "muted",
   size = 18,
   className = "",
   Icon,
 }: IconProps) => {
   const iconVariants = {
-    gray: "text-gray-500",
+    muted: "text-gray-500",
+    social:
+      "w-8 h-8 p-2 rounded-full bg-gray-700 text-white hover:bg-gray-600 transition-colors",
   };
 
-  return <Icon size={size} className={`${iconVariants[type]} ${className}`} />;
+  return (
+    <Icon
+      size={size}
+      className={`${iconVariants[variant]} ${className}`}
+    />
+  );
 };
