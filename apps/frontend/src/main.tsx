9da4f142f11568f6aec8abfd547acd4a72c93708
Vkpro55 <vinodrao835@gmail.com>
2026-05-28 17:08:44 +0530

    feat(header): implement custom location detection

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
+import { LocationProvider } from "./context/LocationContext.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LocationProvider>
+      <App />
+    </LocationProvider>
   </StrictMode>,
 );
