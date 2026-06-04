f5f48dc367f4019b26805d10b9989f42aad69f15
Vkpro55 <vinodrao835@gmail.com>
2026-06-04 22:04:27 +0530

    fix(lint): fix the lint workflow
    fix(lint): fix the lint workflow to enable prisma client
    
    fix(db): fix the prisma.config for github runner
    
    fix(db): fix the prisma.config for github runner
    
    fix(db): fix the prisma.config for github runner
    
    fix(lint): fix the lint workflow to enable prisma client

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,9 +1,6 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-console.log("prisma.config.ts CWD: ", process.cwd());
-console.log("prisma.config.ts DB URL: ", process.env.DATABASE_URL);
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
