1445fa942fb58bbc7f28da29e3569cc8fb0396be
Vkpro55 <vinodrao835@gmail.com>
2026-06-11 10:10:34 +0530

    refactor(ui): add onClick navigation to movie card

--- apps/frontend/src/components/movies/MovieCard.tsx
+++ apps/frontend/src/components/movies/MovieCard.tsx
@@ -10,15 +10,32 @@
 //     age?: string;
 // }
 
+import { useNavigate } from "react-router-dom";
 import type { Movie } from "../../api/types"
+import { useLocation } from "../../context/LocationContext";
 
 // interface IMovieCardProps {
 //     movie: Movie;
 // }
 
 function MovieCard({ movie }: { movie: Movie }) {
+
+    const navigate = useNavigate();
+    const { location } = useLocation();
+
+    const handleNavigate = (movie: Movie) => {
+        const slug = movie.title
+            .toLowerCase()
+            .replace(/[^a-z0-9]+/g, "-")
+            .replace(/^-|-$/g, "");
+
+        void navigate(
+            `/movies/${location}/${slug}/${movie.id}/ticket`,
+        );
+    };
+
     return (
-        <div className="w-40 md:w-52 cursor-pointer">
+        <div className="w-40 md:w-52 cursor-pointer" onClick={() => handleNavigate(movie)}>
             <img src={movie.posterUrl} alt={movie.title} className="rounded-lg shadow-md" />
             <p className="font-medium mt-2">{movie.title}</p>
             <p className="text-xs text-gray-500">{movie.rating} | {movie.votes}</p>
